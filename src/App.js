c2507d1e62bebec62cfba83f2ad831e34f7474a7
Joshua Ling <joshling1919@gmail.com>
2019-06-26 13:03:21 -0700

    Add quiz us ui kit and react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import { QuizUsTheme, QuestionForm } from '@quiz-us/kit';
+
+const standards = [
+  { id: 1, name: '6.5A' },
+  { id: 2, name: '8.5A' },
+  { id: 3, name: '8.8A' }
+];
+
+const questionTypes = ['Free Response'];
 
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <ThemeProvider theme={QuizUsTheme}>
+      <QuestionForm standards={standards} questionTypes={questionTypes} />
+    </ThemeProvider>
+  )
 }
 
 export default App;
