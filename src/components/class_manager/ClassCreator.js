b97de2675e33ea8b922c3481da325882d531fdff
Joshua Ling <joshling1919@gmail.com>
2019-08-24 11:16:10 -0700

    Set up frontend for creating class periods

--- src/components/class_manager/ClassCreator.js
+++ src/components/class_manager/ClassCreator.js
@@ -1,23 +1,62 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import Divider from '@material-ui/core/Divider';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '20px',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  form: {
+    display: 'flex'
+  },
+  createButton: {}
+}));
+
+const Form = ({ classes }) => {
+  return (
+    <form className={classes.form}>
+      <TextField
+        label="Class Name"
+        type="text"
+        InputProps={{
+          endAdornment: (
+            <React.Fragment>
+              <IconButton color="primary" aria-label="Create Class">
+                <AddCircleIcon />
+              </IconButton>
+            </React.Fragment>
+          )
+        }}
+      />
+    </form>
+  );
+};
 
 const ClassCreator = () => {
+  const classes = useStyles();
   const [activated, setActivated] = useState(false);
-  if (activated) {
-    return (
-      <form>
-        <input type="text" />
-      </form>
-    );
-  }
   return (
-    <Button
-      color="primary"
-      variant="contained"
-      onClick={() => setActivated(true)}
-    >
-      Create class
-    </Button>
+    <div className={classes.root}>
+      {activated ? (
+        <Form classes={classes} />
+      ) : (
+        <Button
+          className={classes.createButton}
+          color="secondary"
+          variant="contained"
+          onClick={() => setActivated(true)}
+        >
+          Create class
+        </Button>
+      )}
+    </div>
   );
 };
 
--- src/components/class_manager/ClassManager.js
+++ src/components/class_manager/ClassManager.js
@@ -1,23 +1,63 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
 import ClassCreator from './ClassCreator';
-const useStyles = makeStyles({
-  root: {}
-});
 
-const ClassManager = () => {
-  const classes = useStyles();
+const dummyClasses = [
+  {
+    name: 'Period 1'
+  },
+  {
+    name: 'Period 2'
+  },
+  {
+    name: 'Period 3'
+  }
+];
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: '40px'
+  },
+  list: {
+    backgroundColor: theme.palette.background.paper
+  }
+}));
+
+const Period = ({ name }) => {
   return (
-    <div className={classes.root}>
-      Nothing here yet!
-      {/* <div></div>
-      <ul>
-        <li>Existing class 1</li>
-        <li>Existing class 2</li>
-      </ul>
-      <ClassCreator /> */}
-    </div>
+    <ListItem button>
+      <ListItemText primary={name} />
+    </ListItem>
   );
 };
 
-export default ClassManager;
+export default function SwitchListSecondary() {
+  const classes = useStyles();
+  const numClasses = dummyClasses.length;
+  return (
+    <div className={classes.root}>
+      <h3>Your Classes</h3>
+      {numClasses ? (
+        <List className={classes.list}>
+          {dummyClasses.map((period, i) => {
+            const isNotLastListedClass = numClasses - 1 !== i;
+            return (
+              <React.Fragment>
+                <Period name={period.name} />
+                {isNotLastListedClass && <Divider />}
+              </React.Fragment>
+            );
+          })}
+        </List>
+      ) : (
+        <div>Create your first class!</div>
+      )}
+
+      <ClassCreator />
+    </div>
+  );
+}
