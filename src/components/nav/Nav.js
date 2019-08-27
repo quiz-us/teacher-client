3e61f40069683b33288e476e8b42f78690db1e8b
Joshua Ling <joshling1919@gmail.com>
2019-08-27 16:50:57 -0700

    Remove unused variables

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import Drawer from '@material-ui/core/Drawer';
 import Divider from '@material-ui/core/Divider';
