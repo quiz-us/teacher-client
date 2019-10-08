c2127b2274e0013f794022951b690e5d57c57c3a
Joshua Ling <joshling1919@gmail.com>
2019-10-08 08:03:08 -0700

    Add ClassRoster test

---
+++ src/components/class_manager/ClassRoster.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { MockedProvider } from '@apollo/react-testing';
+import { render } from '@testing-library/react';
+import { MemoryRouter, Route } from 'react-router-dom';
+import faker from 'faker';
+
+// The component AND the query need to be exported
+import ClassRoster from './ClassRoster';
+import { GET_STUDENTS } from '../queries/Student';
+
+const {
+  name: { firstName, lastName },
+  internet: { email },
+  random: { uuid }
+} = faker;
+
+const generateMocked = data => [
+  {
+    request: {
+      query: GET_STUDENTS
+    },
+    result: {
+      data
+    }
+  }
+];
+
+describe('<ClassRoster />', () => {
+  let findByText;
+  const mockedFirstName = firstName();
+  beforeEach(() => {
+    const mockedData = {
+      students: [
+        {
+          firstName: mockedFirstName,
+          lastName: lastName(),
+          email: email(),
+          id: 1,
+          qrCode: uuid()
+        }
+      ]
+    };
+    const mocks = generateMocked(mockedData);
+    ({ findByText } = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <MemoryRouter initialEntries={['/class-manager/1']}>
+          <Route exact path="/class-manager/1" component={ClassRoster} />
+        </MemoryRouter>
+      </MockedProvider>
+    ));
+  });
+
+  it('renders without error', async () => {
+    const result = await findByText(mockedFirstName);
+    expect(result.nodeName).toEqual('TD');
+  });
+});
