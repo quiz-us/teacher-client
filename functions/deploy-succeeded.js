672cef6405518430438c95bfcf120f87b5eaa28f
Joshua Ling <joshling1919@gmail.com>
2019-11-09 10:36:36 -0800

    Try sending post request

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,3 +1,16 @@
+const https = require('https');
+
+const options = { method: 'POST' };
+
 exports.handler = function(event, context, callback) {
+  const { branch } = event;
+
   console.log(event);
+  console.log('BRANCH IS', branch);
+
+  https.request(
+    'https://hooks.zapier.com/hooks/catch/5702449/o2hqohr/',
+    options,
+    res => console.log('RESPONSE WAS', res)
+  );
 };
