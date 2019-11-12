88a93fbccd547f4046703b5497dbb5789f0248c1
Joshua Ling <joshling1919@gmail.com>
2019-11-11 17:53:51 -0800

    Add ability to delete method

--- src/components/class_manager/ClassEdit.js
+++ src/components/class_manager/ClassEdit.js
@@ -6,8 +6,10 @@ import CloseIcon from '@material-ui/icons/Close';
 import CheckIcon from '@material-ui/icons/Check';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import GlobalLoader from '../app/GlobalLoader';
-import { GET_PERIOD, EDIT_PERIOD } from '../queries/Period';
+import { GET_PERIOD, EDIT_PERIOD, DELETE_PERIOD } from '../queries/Period';
 import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import ErrorModal from '.././app/ErrorModal';
 
 const useStyles = makeStyles(theme => ({
   heading: {
@@ -32,7 +34,7 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-const ClassEdit = ({ match }) => {
+const ClassEdit = ({ history, match }) => {
   const classes = useStyles();
   const [editMode, setEditMode] = useState(false);
   const [editableName, setEditableName] = useState('');
@@ -43,10 +45,16 @@ const ClassEdit = ({ match }) => {
       setEditableName(period.name);
     },
   });
+  const [error, setError] = useState(null);
 
   const [editPeriod] = useMutation(EDIT_PERIOD, {
     onCompleted: () => setEditMode(false),
-    onError: err => console.error(err),
+    onError: err => setError(err),
+  });
+
+  const [deletePeriod] = useMutation(DELETE_PERIOD, {
+    onCompleted: () => history.push('/class-manager'),
+    onError: err => setError(err),
   });
 
   if (loading) {
@@ -97,9 +105,25 @@ const ClassEdit = ({ match }) => {
             >
               <EditIcon />
             </IconButton>
+            <IconButton className={classes.iconButtons} title="delete class">
+              <DeleteIcon
+                onClick={() => {
+                  if (
+                    window.confirm(
+                      'Are you sure you want to delete this class? This will unenroll all students and then delete this class!'
+                    )
+                  ) {
+                    deletePeriod({
+                      variables: { periodId: params.id },
+                    });
+                  }
+                }}
+              />
+            </IconButton>
           </div>
         </React.Fragment>
       )}
+      <ErrorModal error={error} />
     </div>
   );
 };
