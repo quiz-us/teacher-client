bfa58ee0dcf048e387695589830fe6418a7ef699
Cynthia Ma <cma@zendesk.com>
2019-11-14 18:36:00 -0800

    Add ability to upload images and load images

---
+++ src/components/deck_creator/QuestionForm/__tests__/EditForm.test.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from '@testing-library/react';
+import QuestionForm from '../index';
+
+jest.mock('slate-plain-serializer', () => {
+  return {
+    serialize: inputVal => {
+      return inputVal;
+    }
+  };
+});
+
+jest.mock('../components/QuestionAndAnswers.js', () => () => {
+  function handleChange(event) {}
+  return <input onChange={handleChange} data-testid='question-rich-editor' />;
+});
+
+jest.mock(
+  '@material-ui/core/Select',
+  () => ({ children, value, onChange, inputProps }) => {
+    function handleChange(event) {
+      onChange(event);
+    }
+    return (
+      <select data-testid={inputProps.id} value={value} onChange={handleChange}>
+        {children.map(({ props: { children, value } }) => (
+          <option key={value} value={value}>
+            {children}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
+
+describe('<EditForm />', () => {
+    const questionTypes = ['Multiple Choice', 'Free Response'];
+    const standards = [
+      { id: 1, name: 'Standard 1' },
+      { id: 2, name: 'Standard 2' }
+    ];
+    
+})
