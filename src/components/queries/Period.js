dc606b97a40c254a1bf91e9f251b025ac531b2bb
Joshua Ling <joshling1919@gmail.com>
2019-12-17 08:23:17 -0800

    Add ClassMastery

--- src/components/queries/Period.js
+++ src/components/queries/Period.js
@@ -55,3 +55,17 @@ export const GET_PERIOD_MASTERY = gql`
     }
   }
 `;
+
+export const GET_PERIOD_SUMMARY = gql`
+  query getPeriodSummary($periodId: ID!) {
+    periodStandardsSummary(periodId: $periodId) {
+      standard {
+        title
+        description
+      }
+      numCorrect
+      numAttempts
+      percentCorrect
+    }
+  }
+`;
