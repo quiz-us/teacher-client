76e1ce706a21cce09a647a215823dc38964e725c
Joshua Ling <joshling1919@gmail.com>
2020-01-25 14:55:17 -0800

    Clone server code in script

--- scripts/cypress.sh
+++ scripts/cypress.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# checkout server staging branch
+# checkout server staging branch unless we are on master/prod, in which case we would pull master server branch
 # launch the database
 # start rails server
 # npm install
 # npm start
 # run cypress
 
-echo "I AM RUNNING"
+git clone https://github.com/quiz-us/quiz-us-server.git
