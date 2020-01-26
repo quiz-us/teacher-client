eb812e7d76b8cb1340ad56f41691c205806e1b75
Joshua Ling <joshling1919@gmail.com>
2020-01-25 16:19:17 -0800

    Remove sleep from cypress script

--- scripts/cypress.sh
+++ scripts/cypress.sh
@@ -13,7 +13,8 @@ cd quiz-us-server
 gem install bundler:2.0.2
 bundle update --bundler
 bundle install --jobs=4 --retry=3 --path vendor/bundle
-bundle exec rake db:setup
+bundle exec rake db:create
+bundle exec rake db:schema:load
 bundle exec rake db:seed
 echo "Database is set up!"
 bin/rails server -d
@@ -22,6 +23,5 @@ bin/rails server -d
 cd ..
 npm run build
 echo "Creating react production build"
-while [ ! -f /build/index.html ]; do sleep 1; done
 npx serve --single build -l 8000 &
 node_modules/.bin/cypress run
