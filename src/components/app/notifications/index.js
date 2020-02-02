1e0139848bc7355e674836a0a1c5cf5c425d4d44
Joshua Ling <joshling1919@gmail.com>
2020-02-01 23:01:15 -0800

    Clean up Notifications

--- src/components/app/notifications/index.js
+++ src/components/app/notifications/index.js
@@ -3,18 +3,12 @@ import NotificationSnackbar from './NotificationSnackbar';
 import NotificationsDialog from './NotificationsDialog';
 import NotificationsConfirmation from './NotificationsConfirmation';
 
-const Notifications = () => {
-  // const {
-  //   notifications: { snack, dialog },
-  // } = useContext(NotificationsContext);
-
-  return (
-    <React.Fragment>
-      <NotificationsConfirmation />
-      <NotificationsDialog />
-      <NotificationSnackbar />
-    </React.Fragment>
-  );
-};
+const Notifications = () => (
+  <React.Fragment>
+    <NotificationsConfirmation />
+    <NotificationsDialog />
+    <NotificationSnackbar />
+  </React.Fragment>
+);
 
 export default Notifications;
